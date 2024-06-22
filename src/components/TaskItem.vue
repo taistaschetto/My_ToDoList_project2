b5f259f702481ace0a224050961c5a3b91baf1a4
Tais Taschetto <taistaschetto@hotmail.com>
2024-06-22 12:57:22 +0200

    task completed goes to end of the list

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -2,13 +2,16 @@
   <div>
     <div v-if="isEditing">
       <input type="text" v-model="editTitle" placeholder="Title" />
-      <input type="text" v-model="editDescription" placeholder="Description" />
       <button @click="saveTask">Save</button>
       <button @click="cancelEdit">Cancel</button>
     </div>
     <div v-else>
-      <input type="checkbox" v-model="task.completed" @change="toggleTaskCompletion">
-      {{ task.title }} - {{ task.description }}
+      <input
+        type="checkbox"
+        v-model="task.is_complete"
+        @change="toggleTaskCompletion"
+      />
+      {{ task.title }}
       <button @click="deleteTask(task.id)">Delete</button>
       <button @click="enableEdit">Edit</button>
     </div>
@@ -16,37 +19,45 @@
 </template>
 
 <script setup>
-import { defineProps, ref } from 'vue';
-import { useTaskStore } from '@/stores/task';
+import { defineProps, ref } from "vue";
+import { useTaskStore } from "@/stores/task";
 
 const props = defineProps({
   task: Object,
 });
 const store = useTaskStore();
 const isEditing = ref(false);
-const editTitle = ref('');
-const editDescription = ref('');
+const editTitle = ref("");
 
 const enableEdit = () => {
   isEditing.value = true;
-  editTitle.value = props.task.title; 
-  editDescription.value = props.task.description; 
+  editTitle.value = props.task.title;
 };
 
 const saveTask = async () => {
-  await store.modifyTask(props.task.id, { title: editTitle.value, description: editDescription.value });
+  await store.modifyTask(props.task.id, {
+    title: editTitle.value,
+  });
   isEditing.value = false;
 };
 
-const toggleTaskCompletion = async() => {
-  store.modifyTask(props.task.id, { completed: !props.task.completed });
+const toggleTaskCompletion = async () => {
+  store.modifyTask(props.task.id, { is_complete: !props.task.is_complete });
 };
 
 const cancelEdit = () => {
-  isEditing.value = false; 
+  isEditing.value = false;
 };
 
 const deleteTask = async (id) => {
   store.deleteTask(id);
 };
 </script>
+
+
+<style scoped>
+.completed {
+  text-decoration: line-through;
+  color: #b3b3b3; 
+}
+</style>
