9b8bfbfc1493dc3fb83b6cb89ddbb0f177d69671
Tais Taschetto <taistaschetto@hotmail.com>
2024-06-25 19:47:10 +0200

    Header styled

--- src/App.vue
+++ src/App.vue
@@ -8,3 +8,11 @@
 <script setup>
 import AppHeader from "@/components/AppHeader.vue";
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap');
+* {
+  background-color: #ffd3d469; 
+  font-family: 'Noto Sans';
+}
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,13 @@
 <template>
   <header class="header">
     <img
-      src="../assets/src/assets/33618b092718aa8ff66fbafd08bf2cc7.jpg"
+      src="../assets/Annotation 2024-06-25 192203.png"
       class="logo"
       alt="My ToDo List Logo"
     />
     <router-link to="/" class="title">My ToDo List</router-link>
     <nav>
       <ul>
-        <!-- Not logged -->
-        <!-- <li v-if="!userStore.user" @click="goTo('Auth')">Sign in / Sign up</li> -->
-
-        <!-- logged in-->
         <li v-if="userStore.user" @click="signOut">Sign Out</li>
       </ul>
     </nav>
@@ -36,45 +32,39 @@ const signOut = async () => {
 </script>
 
 <style scoped>
-
 @media (min-width: 600px) {
   .logo {
-    width: 80px; /* Larger logo for desktop */
+    width: 80px;
   }
-  /* Increase font size or padding as needed for desktop view */
   .title {
-    font-size: 24px; /* Example adjustment */
+    font-size: 40px;
   }
   nav ul li {
-    margin-left: 40px; /* More space between nav items */
+    margin-left: 60px;
   }
 }
 .header {
+  background-color: #ffd3d4;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 1rem;
-  background-color: #ffffff; /* Example background color */
-}
-.logo {
-  width: 50px; /* Adjust as necessary */
-}
-.title {
-  margin: 0 auto;
-  text-decoration: none;
-  color: #333; /* Adjust font color as needed */
-}
-nav ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-nav ul li {
-  margin-left: 20px;
-  cursor: pointer;
-}
-/* Style for screens wider than 600px */
-@media (min-width: 600px) {
-  /* Adjust styles for bigger screens here */
+  .logo {
+    width: 70px;
+  }
+  .title {
+    margin: 0 auto;
+    text-decoration: none;
+    color: #000000; 
+  }
+  nav ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  nav ul li {
+    margin-left: 20px;
+    cursor: pointer;
+  }
 }
 </style>
